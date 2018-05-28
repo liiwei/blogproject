7fd7d85043ce87aba461125331f1e5f65690c06b
liiwei <39696386+liiwei@users.noreply.github.com>
2018-05-28 19:14:42 +0800

    Update settings.py

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -23,7 +23,7 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static')]
 SECRET_KEY = '*1cy=ore8xmgp28qbsadl+_7#@=8^l150ul=jtox7*sjw(&-&3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.waleli.top']
 
